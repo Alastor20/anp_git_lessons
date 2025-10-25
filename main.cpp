852ec2acb5594bf392487b0f04718d286890ed5d
Alastor20 <nik_d.007@mail.ru>
2025-10-25 14:25:22 +0300

    EOF and FAIL check

--- main.cpp
+++ main.cpp
@@ -5,4 +5,13 @@ int main()
     using u_t = unsigned;
     u_t a = 0;
     std::cin >> a;
+    if (std::cin.eof())
+    {
+        std::cout << 0 << '\n';
+    }
+    else if (std::cin.fail())
+    {
+        std::cerr << "ERROR" << '\n';
+        return 1;
+    }
 }
