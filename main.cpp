9cc834239fb517c8be95e66ee2e0ed6d9e477e6c
Alastor20 <nik_d.007@mail.ru>
2025-10-25 14:36:23 +0300

    counting amount of numbers

--- main.cpp
+++ main.cpp
@@ -2,16 +2,21 @@
 
 int main()
 {
-    using u_t = unsigned;
-    u_t a = 0;
-    std::cin >> a;
-    if (std::cin.eof())
-    {
-        std::cout << 0 << '\n';
-    }
-    else if (std::cin.fail())
-    {
-        std::cerr << "ERROR" << '\n';
-        return 1;
-    }
+  using u_t = unsigned;
+  u_t a = 0;
+  size_t count = 0;
+  while (std::cin >> a)
+  {
+    ++count;
+  }
+
+  if (std::cin.eof())
+  {
+    std::cout << count << '\n';
+  }
+  else if (std::cin.fail())
+  {
+    std::cerr << "ERROR" << '\n';
+    return 1;
+  }
 }
