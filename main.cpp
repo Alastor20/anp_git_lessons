439d180470f615ec5d537d0bee31ea9522f8d59d
Alastor20 <nik_d.007@mail.ru>
2025-10-25 15:00:54 +0300

    task completed

--- main.cpp
+++ main.cpp
@@ -7,9 +7,13 @@ int main()
   using u_t = unsigned;
   u_t a = 0, b = 0, c = 0;
   size_t count = 0;
+  std::cin >> c;
+  std::cin >> b;
   while (std::cin >> a)
   {
     count += isPyth(a, b, c) ? 1 : 0;
+    c = b;
+    b = a;
   }
 
   if (std::cin.eof())
